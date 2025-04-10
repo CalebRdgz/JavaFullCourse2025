e590c36e4d46eea87c635f2830fbd0d6aad5b8a9
Caleb Rodriguez <calebrodgz@gmail.com>
2025-04-10 15:41:00 -0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,18 @@ public class Main {
 
     public static void main(String[] args){ //need this method so that our program can run
 
+        //THIS IS MY FIRST JAVA PROGRAM
+
+        /*
+        This
+        is
+        a
+        multi-line
+        comment
+         */
+
+        System.out.println("I like pizza!");
+        System.out.println("It's really good!");
+        System.out.println("Buy me pizza!");
     }
 }
