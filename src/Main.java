92bb02d9a6da28c80db418aaa11a6f6e05f6cb2b
Caleb Rodriguez <calebrodgz@gmail.com>
2025-04-22 13:29:11 -0500

    user input

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){ //need this method so that our program can run
@@ -11,49 +12,115 @@ public class Main {
         multi-line
         comment
          */
+        //2. VARIABLES
+//        System.out.println("I like pizza!");
+//        System.out.println("It's really good!");
+//        System.out.println("Buy me pizza!");
+//
+//        //2 steps to creating a variable
+//        //1. declaration
+//        //2, assignment
+//
+//        int age = 30;
+//        int year = 2025;
+//        int quantity = 1;
+//
+//        double price = 19999.99; //double contains decimal
+//        double gpa = 3.5;
+//        double temperature = -12.5;
+//
+//        char grade = 'A';
+//        char symbol = '!';
+//        char currency = '$';
+//
+//        boolean isStudent = false;
+//        boolean forSale = true;
+//        boolean isOnline = true;
+//
+//        String name = "Caleb";
+//        String food = "pizza";
+//        String email = "fake123@gmail.com";
+//        String car = "Mustang";
+//        String color = "red";
+//
+//        System.out.println("Hello " + name);
+//        System.out.println("You are " + age + " years old");
+//        System.out.println("Hello " + name + ", your favorite food is: " + food);
+//        System.out.println("Your gpa is: " + gpa);
+//        System.out.println("Your average letter grade is: " + grade);
+//        System.out.println("Your choice is a " + color + " " + year + " " + car);
+//        System.out.println("The price is: " + currency + price);
+//
+//        if (forSale){
+//            System.out.println("There is a " + car + " for sale");
+//        }else {
+//            System.out.println("The " + car + " is not for sale");
+//        }
+        //3. USER INPUT
+        //(Scanner name of class) (scanner name of object)
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter your name: ");
+//        String name = scanner.nextLine(); //take the user input as "name"
+//
+//        System.out.print("Enter your age: ");
+//        int age = scanner.nextInt();
+//
+//        System.out.print("What is your gpa: ");
+//        double gpa = scanner.nextDouble();
+//
+//        System.out.print("Are you a student? (true or false): ");
+//        boolean isStudent = scanner.nextBoolean();
+//
+//        System.out.println("Hello " + name);
+//        System.out.println("You are " + age + " years old.");
+//        System.out.println("Your gpa is: " + gpa);
+//
+//        if(isStudent){
+//            System.out.println("You are enrolled as a student");
+//        }else{
+//            System.out.println("You are NOT enrolled");
+//        }
+//
+//        scanner.close(); //have to close the scanner after using it
 
-        System.out.println("I like pizza!");
-        System.out.println("It's really good!");
-        System.out.println("Buy me pizza!");
+        //COMMON ISSUES - accepting an int/double, then accepting a string:
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter your age: ");
+//        int age = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        System.out.print("Enter your favorite color: "); //breaks by taking in the "/n" after entering age^
+//        String color = scanner.nextLine();
+//
+//        System.out.println("You are " + age + " years old.");
+//        System.out.println("You like the color " + color);
+//
+//        scanner.close();
 
-        //2 steps to creating a variable
-        //1. declaration
-        //2, assignment
+//        //Scanner exercise
+//        //reassign these variables with user input:
+//        double width = 0;
+//        double height = 0;
+//        double area = 0;
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter the width: ");
+//        width = scanner.nextDouble();
+//
+//        System.out.print("Enter the height: ");
+//        height = scanner.nextDouble();
+//
+//        //calculate the area:
+//        area = width*height;
+//
+//        System.out.println("The area is: " + area + "cm^2");
+//
+//        scanner.close();
 
-        int age = 30;
-        int year = 2025;
-        int quantity = 1;
+        //
 
-        double price = 19999.99; //double contains decimal
-        double gpa = 3.5;
-        double temperature = -12.5;
-
-        char grade = 'A';
-        char symbol = '!';
-        char currency = '$';
-
-        boolean isStudent = false;
-        boolean forSale = true;
-        boolean isOnline = true;
-
-        String name = "Caleb";
-        String food = "pizza";
-        String email = "fake123@gmail.com";
-        String car = "Mustang";
-        String color = "red";
-
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age + " years old");
-        System.out.println("Hello " + name + ", your favorite food is: " + food);
-        System.out.println("Your gpa is: " + gpa);
-        System.out.println("Your average letter grade is: " + grade);
-        System.out.println("Your choice is a " + color + " " + year + " " + car);
-        System.out.println("The price is: " + currency + price);
-
-        if (forSale){
-            System.out.println("There is a " + car + " for sale");
-        }else {
-            System.out.println("The " + car + " is not for sale");
-        }
     }
 }
