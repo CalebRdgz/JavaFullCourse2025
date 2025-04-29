bdb3cac4827a270ce875394ad1348d62b731f0b4
Caleb Rodriguez <calebrodgz@gmail.com>
2025-04-29 11:24:17 -0500

    weight converter

--- src/Main.java
+++ src/Main.java
@@ -509,27 +509,67 @@ public class Main {
         // string.substring(start, end)
 
         //email slicer program:
-        String email;
-        String username;
-        String domain;
+//        String email;
+//        String username;
+//        String domain;
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter your email: ");
+//        email = scanner.nextLine();
+//
+//        if(email.contains("@")){
+//            username = email.substring(0, email.indexOf("@")); //get everything before the @
+//            domain = email.substring(email.indexOf("@") + 1); //get everything after the @
+//
+//            System.out.printf("Your username: %s\n", username);
+//            System.out.printf("Your domain: %s\n", domain);
+//        }
+//        else{
+//            System.out.println("Emails must contain @");
+//        }
+//
+//        scanner.close();
 
+        //WEIGHT CONVERSION PROGRAM
+
+        //Declare variables
         Scanner scanner = new Scanner(System.in);
+        double weight;
+        double newWeight;
+        int choice;
 
-        System.out.print("Enter your email: ");
-        email = scanner.nextLine();
+        //Welcome message
+        System.out.println("Weight conversion program");
+        System.out.println("1: Convert lbs to kgs");
+        System.out.println("2: Convert kgs to lbs");
 
-        if(email.contains("@")){
-            username = email.substring(0, email.indexOf("@")); //get everything before the @
-            domain = email.substring(email.indexOf("@") + 1); //get everything after the @
-
-            System.out.printf("Your username: %s\n", username);
-            System.out.printf("Your domain: %s\n", domain);
+        //prompt for user choice
+        System.out.print("Choose an option: ");
+        choice = scanner.nextInt();
+        //option 1: convert lbs to kgs
+        if(choice == 1){
+            System.out.print("Enter the weight in lbs: ");
+            weight = scanner.nextDouble();
+            newWeight = weight * 0.453592;
+            System.out.printf("The new weight in kgs is: %.2f\n", newWeight);
         }
+        //option 2: convert kgs to lbs
+        else if(choice == 2){
+            System.out.print("Enter the weight in kgs: ");
+            weight = scanner.nextDouble();
+            newWeight = weight * 2.20462;
+            System.out.printf("The new weight in lbs is: %.2f\n", newWeight);
+        }
+        //else print: not a valid choice≈
         else{
-            System.out.println("Emails must contain @");
+            System.out.println("That was not a valid choice");
         }
+
+
 
         scanner.close();
+
 
     }
 }
