65ddeb94eba517779ca9aba4ec16bcabc863e093
Caleb Rodriguez <calebrodgz@gmail.com>
2025-04-30 15:20:34 -0500

    Calculator program

--- src/Main.java
+++ src/Main.java
@@ -678,6 +678,51 @@ public class Main {
 //
 //        scanner.close();
 
-        //
+        //CALCULATOR PROGRAM
+        Scanner scanner = new Scanner(System.in);
+
+        double num1;
+        double num2;
+        char operator;
+        double result = 0;
+        boolean validOperation = true;
+
+        System.out.print("Enter the first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter an operator: (+, -, *, /, ^): ");
+
+        operator = scanner.next().charAt(0); //just get the first letter of user input, keeping it a char instead of a string
+
+        System.out.print("Enter the second number: ");
+        num2 = scanner.nextDouble();
+
+        //enhanced switch to examine user operator against our cases:
+        switch (operator){
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if(num2 == 0){
+                    System.out.println("Cannot divide by zero!");
+                    validOperation = false;
+                }
+                else{
+                    result = num1 / num2;
+                }
+            }
+            case '^' -> result = Math.pow(num1, num2);
+            default -> {
+                System.out.println("Invalid operator!");
+                validOperation = false;
+            }
+        }
+
+        if(validOperation){
+            System.out.println(result);
+        }
+
+        scanner.close();
+
     }
 }
