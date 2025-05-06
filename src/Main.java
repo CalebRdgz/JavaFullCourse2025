bc740ea06e3bec850886e7da1243bb852ae3cda0
Caleb Rodriguez <calebrodgz@gmail.com>
2025-05-06 14:17:14 -0500

    nested loops

--- src/Main.java
+++ src/Main.java
@@ -918,14 +918,68 @@ public class Main {
         // break = break out of a loop (STOP)
         // continue = skip current iteration of a loop (SKIP)
 
-        for(int i = 0; i < 10; i++){
+//        for(int i = 0; i < 10; i++){
+//
+//            if(i == 5){
+//                continue;
+//            }
+//
+//            System.out.print(i + " ");
+//        }
 
-            if(i == 5){
-                continue;
+        // nested loop = A loop inside another loop
+        // Used often with matrices or Data Structures and Algorithms
+
+//        for(int i = 1; i <= 9; i++){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//        for(int i = 1; i <= 9; i++){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//        for(int i = 1; i <= 9; i++){
+//            System.out.print(i + " ");
+//        }
+
+        //DONT REPEAT CODE^^^
+
+//        for(int i = 1; i <= 3; i++){
+//
+//            for(int j = 1; j <= 9; j++){
+//                System.out.print(j + " ");
+//            }
+//            System.out.println();
+//        }
+
+        //create a matrix of a symbol that user types in. user sets rows and columns of matrix
+
+        Scanner scanner = new Scanner(System.in);
+
+        int rows;
+        int columns;
+        char symbol;
+
+        System.out.print("Enter the # of rows: ");
+        rows = scanner.nextInt();
+
+        System.out.print("Enter the # of columns: ");
+        columns = scanner.nextInt();
+
+        System.out.print("Enter the symbol to use: ");
+        symbol = scanner.next().charAt(0);
+
+        for(int i = 0; i < rows; i++){
+
+            for(int j = 0; j < columns; j++){
+                System.out.print(symbol);
             }
-
-            System.out.print(i + " ");
+            System.out.println();
         }
+
+
+        scanner.close();
+
 
     }
 }
