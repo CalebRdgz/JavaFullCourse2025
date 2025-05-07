145f9deed310057522ddab7fd27e9fd72502307f
Caleb Rodriguez <calebrodgz@gmail.com>
2025-05-07 16:03:56 -0500

    search an array

--- src/Main.java
+++ src/Main.java
@@ -1159,7 +1159,35 @@ public class Main {
 ////        System.out.println(foods.length);
 //        scanner.close();
 
+        //search an array:
+        Scanner scanner = new Scanner(System.in);
 
+
+        int[] numbers = {1, 9, 2, 8, 3, 5, 4};
+        String[] fruits = {"apple", "orange", "banana"};
+        boolean isFound = false;
+        String target;
+//        int target = 1;
+
+        while(!isFound){
+            System.out.print("Enter a fruit to search for: ");
+            target = scanner.nextLine();
+
+            //linear search:
+            for(int i = 0; i < fruits.length; i++){
+                if(fruits[i].equals(target)){
+                    System.out.println("Element found at index: " + i);
+                    isFound = true;
+                    break;
+                }
+            }
+
+            if(!isFound){
+                System.out.println("Element not found in the array");
+            }
+        }
+
+        scanner.close();
     }
     //DISPLAY ASCII ART OF DICE METHOD:
 //    static void printDie(int roll){
